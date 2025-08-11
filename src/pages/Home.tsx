6528241a0eb9b40f8ee595a5c4f550f2948eab3e
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-08-11 16:31:12 -0300

    feat: cria a página Home com componente Banner

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import Banner from '../components/Banner';
+
+const Home = () => {
+  return (
+    <>
+      <Banner
+        title="Bem-vindo ao Sistema da Faculdade!"
+        description="Nosso sistema oferece uma plataforma prática e segura para você acompanhar as atividades acadêmicas, participar das assembleias e exercer seu direito de voto com transparência. Conectando alunos, professores e colaboradores para uma gestão colaborativa e eficiente."
+      />
+    </>
+  );
+};
+
+export default Home;
