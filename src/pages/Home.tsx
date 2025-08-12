3f6888ba05e34e562fc047e829579651c443e01b
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-08-12 15:36:23 -0300

    feat: ajusta estrutura do componente Home para incluir uma div de contêiner

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,12 @@ import Banner from '../components/Banner';
 
 const Home = () => {
   return (
-    <>
+    <div className="dashboard-page">
       <Banner
         title="Bem-vindo ao Sistema da Faculdade!"
         description="Nosso sistema oferece uma plataforma prática e segura para você acompanhar as atividades acadêmicas, participar das assembleias e exercer seu direito de voto com transparência. Conectando alunos, professores e colaboradores para uma gestão colaborativa e eficiente."
       />
-    </>
+    </div>
   );
 };
 
