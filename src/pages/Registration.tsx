a1eb4ce30b0fe9ae4f522995709f5915f7a67cd7
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-08-13 13:53:51 -0300

    feat: adiciona página de Matricula com componente Banner

---
+++ src/pages/Registration.tsx
@@ -0,0 +1,18 @@
+import Banner from '../components/Banner';
+
+const Matriculas = () => {
+  return (
+    <>
+      <Banner
+        title="Gestão de Matrículas"
+        description="Gerencie as matrículas dos alunos nas disciplinas. Visualize inscrições, cancelamentos e histórico acadêmico."
+      />
+      <div className="content">
+        <h2>Sistema de Matrículas</h2>
+        <p>Em desenvolvimento...</p>
+      </div>
+    </>
+  );
+};
+
+export default Matriculas;
