40f4e459eada1c21ebb0900a9ed55c697be5c582
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-08-13 13:41:03 -0300

    feat: adiciona sidebar e reorganiza estrutura do componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,44 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
 import Home from './pages/Home';
+import Student from './pages/Student';
+import Professores from './pages/Professores';
+import Disciplinas from './pages/Disciplinas';
+import Matriculas from './pages/Matriculas';
+import Notas from './pages/Notas';
+import Relatorios from './pages/Relatorios';
 
 const App = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
+  const closeSidebar = () => {
+    setSidebarOpen(false);
+  };
+
   return (
     <BrowserRouter>
       <div className="app">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-        <Footer />
+        <Sidebar isOpen={sidebarOpen} onClose={closeSidebar} />
+        <div className="main-content">
+          <Header onMenuToggle={toggleSidebar} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/alunos" element={<Student />} />
+            <Route path="/professores" element={<Professores />} />
+            <Route path="/disciplinas" element={<Disciplinas />} />
+            <Route path="/matriculas" element={<Matriculas />} />
+            <Route path="/notas" element={<Notas />} />
+            <Route path="/relatorios" element={<Relatorios />} />
+          </Routes>
+          <Footer />
+        </div>
       </div>
     </BrowserRouter>
   );
