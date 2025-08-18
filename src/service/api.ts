85be50c39096bda99146d504ca7bc559e486bf9b
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-08-18 11:32:57 -0300

    feat: adiciona interceptor de requisição para inclusão de token de autenticação

--- src/service/api.ts
+++ src/service/api.ts
@@ -7,6 +7,19 @@ const api = axios.create({
   }
 });
 
+api.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
 api.interceptors.response.use(
   response => response,
   error => {
